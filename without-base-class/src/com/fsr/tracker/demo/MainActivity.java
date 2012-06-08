841d78fdf3f2281e1cb7f40cbf0f226016d80f46
H.Y <hoyan.leung@foreseeresults.com>
2012-06-08 12:05:22 -0700

    Added non-base class based implementation

---
+++ without-base-class/src/com/fsr/tracker/demo/MainActivity.java
@@ -0,0 +1,22 @@
+package com.fsr.tracker.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.fsr.tracker.app.TrackingContext;
+import com.fsr.tracker.demo.R;
+import com.fsr.tracker.domain.*;
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        //Create a configuration to be used by the TrackingContext
+        Configuration configuration = Configuration.defaultConfiguration("7PzwF4wMfCv/r3yXCc0GFw==")
+        		.addMeasure(MeasureConfiguration.defaultConfig("DefaultMeasure", "mobile", 0)
+        				.withMaxLaunchCount(2));
+        TrackingContext.Instance().initialize(this, configuration);
+        TrackingContext.Instance().applicationLaunched();
+    }
+}
