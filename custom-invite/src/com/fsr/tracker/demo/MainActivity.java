f194baa84b7cd27f55d53e6e0029169dab25e93d
H.Y <hoyan.leung@foreseeresults.com>
2012-06-08 14:43:37 -0700

    Added custom invite sample
    Sample app showing customization of invite text and logo.

---
+++ custom-invite/src/com/fsr/tracker/demo/MainActivity.java
@@ -0,0 +1,43 @@
+package com.fsr.tracker.demo;
+
+import com.fsr.tracker.app.DefaultStringsProvider;
+import com.fsr.tracker.app.TrackingContext;
+import com.fsr.tracker.domain.Configuration;
+import com.fsr.tracker.domain.MeasureConfiguration;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        //Create a configuration to be used by the TrackingContext
+        Configuration configuration = Configuration.defaultConfiguration("7PzwF4wMfCv/r3yXCc0GFw==")
+        		.withCustomLogo("acme_logo.jpg")
+        		.addMeasure(MeasureConfiguration.defaultConfig("DefaultMeasure", "mobile", 0)
+        				.withMaxLaunchCount(2));
+        TrackingContext.Instance().initialize(this, configuration,new MyStringsProvider());
+        TrackingContext.Instance().applicationLaunched();
+    }
+    
+    private class MyStringsProvider extends DefaultStringsProvider
+    {
+    	
+
+		@Override
+		public String getInviteTitle() {
+			return "How are we doing?";
+		}
+
+		@Override
+		public String getInviteText() {
+			return "We'd like to invite you to participate in a brief customer satisfaction survey";
+		}
+		
+
+
+    }
+}
