7dc02163b26e1b4b0ea9cc06d90214ead27bd8a8
H.Y <hoyan.leung@foreseeresults.com>
2012-06-08 14:53:48 -0700

    Added Config customization to basic demo
    Added some sample code to show customization of configuration using base classes.

--- base-class/src/com/fsr/tracker/demo/MainActivity.java
+++ base-class/src/com/fsr/tracker/demo/MainActivity.java
@@ -2,7 +2,11 @@ package com.fsr.tracker.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
 import com.fsr.tracker.app.*;
+import com.fsr.tracker.domain.Configuration;
+import com.fsr.tracker.domain.MeasureConfiguration;
 
 public class MainActivity extends TrackerActivityBase {
     /** Called when the activity is first created. */
@@ -20,4 +24,21 @@ public class MainActivity extends TrackerActivityBase {
     protected  String getDefaultSurveyId() {
         return "mobile";
     }
+    
+    public void doSomething(View source)
+    {
+    	TrackingContext.Instance().incrementSignificantEventsCountWithKey("doSomething");
+    }
+
+	@Override
+	protected Configuration getConfiguration() {
+		Configuration config =  super.getConfiguration();
+		//we can customize the configuration by overriding the getConfiguration() method
+		//and adding our own settings
+		return config
+				.addMeasure(MeasureConfiguration.defaultConfig("Measure 2", "mobile", 1)
+						.addSignificantEventThreshold("doSomething", 5));
+	}
+    
+    
 }
