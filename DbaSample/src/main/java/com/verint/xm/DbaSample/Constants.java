f738698cf9b21b69f445217eea6fc65d2141a27d
Donal Ryan <donal.ryan@verint.com>
2023-01-11 12:24:02 +0000

    VOC-35704: Minor code changes

---
+++ DbaSample/src/main/java/com/verint/xm/DbaSample/Constants.java
@@ -0,0 +1,5 @@
+package com.verint.xm.DbaSample;
+
+public class Constants {
+    public static String FCP_VERSION = "mobsdk";
+}
--- DbaSample/src/main/java/com/verint/xm/DbaSample/DbaSampleApplication.java
+++ DbaSample/src/main/java/com/verint/xm/DbaSample/DbaSampleApplication.java
@@ -1,19 +1,41 @@
 package com.verint.xm.DbaSample;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.verint.xm.sdk.Core;
 import com.verint.xm.sdk.DBA;
+import com.verint.xm.sdk.common.Logging;
 import com.verint.xm.sdk.internal.CoreProxy;
 
 
 public class DbaSampleApplication extends Application {
+
+    private final Core.VerintSDKListener verintSDKListener = new Core.VerintSDKListener() {
+        @Override
+        public void onSDKStarted() {
+            Log.i("VerintSDKListener", "onSDKStarted");
+        }
+
+        @Override
+        public void onSDKStarted(Core.VerintError verintError, String errorDescription) {
+            Log.i("VerintSDKListener", "onSDKStarted with error: " + verintError.name() + ": " + errorDescription);
+        }
+
+        @Override
+        public void onSDKFailedToStart(Core.VerintError verintError, String errorDescription) {
+            Log.i("VerintSDKListener", "onSDKFailedToStart with error: " + verintError.name() + ": " + errorDescription);
+        }
+    };
+
     @Override
     public void onCreate() {
         super.onCreate();
         // Regular startup
         Core.setDebugLogEnabled(true);
-        Core.start(this);
+        Core.setSDKListener(verintSDKListener);
+        String appId = getApplicationContext().getPackageName();
+        Core.startWithAppId(this, appId, Constants.FCP_VERSION);
         DBA.setMaskingDebugEnabled(true);
     }
 }
