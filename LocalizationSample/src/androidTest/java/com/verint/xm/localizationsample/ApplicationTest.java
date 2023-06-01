43b9468953889d45c7cec7e88d4d13cdd3f93861
Matthew McNabb <matthew.mcnabb@verint.com>
2023-06-01 20:54:05 +0100

    VOC-31232: Updated all package names

---
+++ LocalizationSample/src/androidTest/java/com/verint/xm/localizationsample/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.verint.xm.localizationsample;
+
+import android.app.Application;
+
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super();
+    }
+}
---
+++ LocalizationSample/src/main/java/com/verint/xm/localizationSample/MainActivity.java
@@ -0,0 +1,34 @@
+package com.verint.xm.localizationSample;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+import com.verint.xm.sdk.Core;
+import com.verint.xm.sdk.Predictive;
+
+public class MainActivity extends AppCompatActivity {
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Do normal UI setup
+        setContentView(R.layout.main);
+     }
+
+    public void launchInvite(View view)
+    {
+        // Increment the significant event count so that we're eligible for an invite
+        // based on the criteria in foresee_configuration.json
+        Predictive.incrementSignificantEventCountWithKey("instant_invite");
+
+        // Launch an invite as a demo
+        Predictive.checkIfEligibleForSurvey();
+    }
+
+    public void resetState(View view) {
+        // Reset the state of the ForeSee SDK
+        Core.resetState();
+    }
+}
