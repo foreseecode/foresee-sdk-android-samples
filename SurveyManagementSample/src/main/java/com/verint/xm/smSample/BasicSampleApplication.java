4d6ab0ee9dbd31ec76d87a85def7152359515653
Donal Ryan <donal.ryan@verint.com>
2023-07-19 16:38:47 +0100

    VOC-43344: Add new SM sample app

---
+++ SurveyManagementSample/src/main/java/com/verint/xm/smSample/BasicSampleApplication.java
@@ -0,0 +1,15 @@
+package com.verint.xm.smSample;
+
+import android.app.Application;
+
+import com.verint.xm.sdk.Core;
+
+public class BasicSampleApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		// Notify ForeSee SDK of application start
+		Core.setDebugLogEnabled(true);
+		Core.start(this);
+	}
+}
---
+++ SurveyManagementSample/src/main/java/com/verint/xm/smSample/MainActivity.java
@@ -0,0 +1,33 @@
+package com.verint.xm.smSample;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+
+import com.verint.xm.sdk.Core;
+import com.verint.xm.sdk.Predictive;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCheckEligibilityClicked(View view) {
+
+        // Launch an invite as a demo
+        Predictive.checkIfEligibleForSurvey();
+    }
+
+    public void onIncrementSigEventClicked(View view) {
+        Predictive.incrementSignificantEventCountWithKey("");
+    }
+
+    public void resetCounters(View view) {
+        // Reset the SDK
+        Core.resetState();
+    }
+}
